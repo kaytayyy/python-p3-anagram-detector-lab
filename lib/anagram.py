95bb982a139a0e8c50c12568b0e8c426fa4f302e
Inda Smith <inda.k.smith@gmail.com>
2024-04-15 01:06:04 -0400

    it worked I guess

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self,words):
+        matches = []
+        for w in words:
+            if w.lower() != self.word and len(w) == len(self.word):
+                if sorted(w.lower()) == sorted(self.word):
+                    matches.append(w)
+        return matches
